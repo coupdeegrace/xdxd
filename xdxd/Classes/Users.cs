270496b8e6c581b7c86e4fc744c3b61abcfe8d8f
Глушков Александр Викторович <avglushkov@phoenixit.ru>
2024-10-23 11:09:00 +0300

    working on log in and sign up functions

---
+++ xdxd/Classes/Users.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Newtonsoft.Json.Converters;
+
+
+namespace xdxd
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
