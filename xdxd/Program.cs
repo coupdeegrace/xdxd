811e04b909bb5b375c37d37cbb96e6a416ef9e6e
coupdeegrace <c0updegr4c3@gmail.com>
2024-10-30 14:05:35 +0300

    awful sign up, rename your variables u idiot

--- xdxd/Program.cs
+++ xdxd/Program.cs
@@ -9,6 +9,7 @@ namespace xdxd
 {
     internal class Program
     {
+        // variables naming are awful, rename later!!!
         static string currentUser; 
         
         static void Main(string[] args)
@@ -24,54 +25,62 @@ namespace xdxd
         static void Login()
         {
             
-            Console.WriteLine("1) Log in your account\n2) Don't have an account? Sign up now.\n3) About test system");
-            int choice = int.Parse(Console.ReadLine()); //exception if user enters non-int variable, fix this u dumbass
-            switch (choice)
+            Console.WriteLine("1) Log in your account\n2) Don't have an account? Sign up now.\n3) About test system");    
+            try
             {
-                case 1: //re-do log in with methods (optimization) -- in process
-                    Console.WriteLine("Insert your login:");
-                    string login = Console.ReadLine();
+                int choice = int.Parse(Console.ReadLine()); //exception if user enters non-int variable, fix this u dumbass | done, very poorly, unlucky
+                switch (choice)
+                {
+                    case 1: //re-do log in with methods (optimization) -- in process
+                        Console.WriteLine("Insert your login:");
+                        string login = Console.ReadLine();
 
-                    //string jsonPath = @"./Users.json";
-                    //string usersJson = File.ReadAllText(jsonPath);
-                    List<Users> User = GetUserFromFile();
+                        //string jsonPath = @"./Users.json";
+                        //string usersJson = File.ReadAllText(jsonPath);
+                        List<Users> User = GetUserFromFile();
 
-                    //bool isUserExist = currentUsers.Exists(user => user.UserName == login);
+                        //bool isUserExist = currentUsers.Exists(user => user.UserName == login);
 
-                    if (IsUserExist(login)) //to much shit is going on here, re-do later
-                    {
-                        Console.WriteLine("Insert your password:");
-                        string password = Console.ReadLine();
-
-                        Users userData = GetUserByUsername(login); //null reference, debug this u dumbass (done)
-                        if (userData.Password == password) 
+                        if (IsUserExist(login)) //to much shit is going on here, re-do later
                         {
-                            currentUser = userData.UserName;
-                            Authorization();
+                            Console.WriteLine("Insert your password:");
+                            string password = Console.ReadLine();
+
+                            Users userData = GetUserByUsername(login); //null reference, debug this u dumbass (done)
+                            if (userData.Password == password)
+                            {
+                                currentUser = userData.UserName;
+                                Authorization();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Password is incorrect.");
+                                Task.Delay(3000);
+                                Login();
+                            }
                         }
                         else
                         {
-                            Console.WriteLine("Password is incorrect.");
+                            Console.WriteLine("Login is incorrect (no users found).");
                             Task.Delay(3000);
                             Login();
                         }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Login is incorrect (no users found).");
-                        Task.Delay(3000);
+                        break;
+                    case 2:
+                        SignUp();
+                        break;
+                    default:
+                        Console.WriteLine("Please, select between following numbers.");
                         Login();
-                    }
-                    break;
-                case 2:
-                    SignUp();
-                    break;
-                default:
-                    Console.WriteLine("Please, select between following numbers.");
-                    Login();
-                    break;
+                        break;
+                }
             }
-            
+            catch (Exception)
+            {
+                Console.WriteLine("Please, select between following numbers.");
+                Login();
+                throw; // sometimes catching wierd shit, idk, will researh it later 
+            }                      
         }
 
         static void Authorization()
@@ -86,17 +95,39 @@ namespace xdxd
             Console.WriteLine("Let's create your account!\nEnter your login:");
             string login = Console.ReadLine();
 
-            string password = Console.ReadLine();
-
 
             ////
-            string jsonPath = @"./Users.json"; //same shit, re-do later
-            string usersJson = File.ReadAllText(jsonPath);
-            List<Users> currentUsers = JsonConvert.DeserializeObject<List<Users>>(usersJson);
+                //string jsonPath = @"./Users.json"; //same shit, re-do later
+                //string usersJson = File.ReadAllText(jsonPath);
+                //List<Users> currentUsers = JsonConvert.DeserializeObject<List<Users>>(usersJson);
             ///// - old 
 
             //// - new
             List<Users> users = GetUserFromFile();
+
+            if (IsUserExist(login))
+            {
+                Console.WriteLine("Current username is taken, please choose a differnt one");
+                Task.Delay(7000);
+                Login();
+            }
+           
+            Console.WriteLine("Enter your password");
+            string password = Console.ReadLine();
+
+            Users newUser = new Users()
+            {
+                UserId = GetNextUserId(),
+                UserName = login,
+                Password = password
+            };
+               
+            List<Users> currentUsers = GetUserFromFile();
+            currentUsers.Add(newUser);
+            AddUsersToFile(currentUsers);
+            Console.WriteLine($"User {newUser.UserName} added successfully, please log in;");
+            Login();
+
         }
        
         static void JsonDeserializer() //will delete in the future -- almost done with replacing the whole method, just ignore it
@@ -118,6 +149,19 @@ namespace xdxd
                 string exception = ex.ToString();
                 Console.WriteLine(exception);
             }
+        }
+
+        static void AddUsersToFile(List<Users> users)
+        {
+            string path = @"./Users.json";
+            string UsersJson = JsonConvert.SerializeObject(users, Formatting.Indented); //wtf is that naming . . . it's confusing even for myself, others will rope themselves after rewiewing my code
+            File.WriteAllText(path, UsersJson);
+        }
+
+        static int GetNextUserId()
+        {
+            List<Users> users = GetUserFromFile();
+            return users.Count > 0 ? users[users.Count - 1].UserId + 1 : 1;
         }
 
         static List<Users> GetUserFromFile()
